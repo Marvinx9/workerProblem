9858d8d8f40d85190fa032ffe83d782e40dc03a5
Marvinx9 <afraniodantas0224@gmail.com>
2025-06-02 14:12:29 -0300

    feat: add work program

--- src/main/java/org/marvin/application/Program.java
+++ src/main/java/org/marvin/application/Program.java
@@ -1,21 +1,42 @@
 package org.marvin.application;
 
+import org.marvin.entities.Department;
 import org.marvin.entities.Worker;
 import org.marvin.entities.enums.WorkerLevel;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Worker order = new Worker(1080, new Date(), WorkerLevel.PENDING_PAYMENT);
 
-        System.out.println(order);
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        WorkerLevel os1 = WorkerLevel.DELIVERED;
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+        System.out.print("Enter worker data: ");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.print("Base salary: ");
+        double baseSalary = sc.nextDouble();
 
-        WorkerLevel os2 = WorkerLevel.valueOf("DELIVERED");
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
 
-        System.out.println(os1);
-        System.out.println(os2);
+        System.out.print("How many contracts to this worker? ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<n; i++){
+            System.out.println("Enter contract #" + i + " data:");
+            System.out.print("Date (DD/MM/YYYY): ");
+            Date contractDate =
+        }
+
+        sc.close();
     }
 }
